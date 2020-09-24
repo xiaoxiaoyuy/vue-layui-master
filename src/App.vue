8d61847a21e56692f1364b0ad5477a6f7c71fd0e
EvenNearBoatWater <1225001172@qq.com>
2020-09-24 09:45:03 +0800

    员工的部件月产量

--- src/App.vue
+++ src/App.vue
@@ -38,7 +38,7 @@
                         <lay-menu-child-item title="安全设置"></lay-menu-child-item>
                     </lay-menu-item>
                     <lay-menu-item :index="0">
-                        <template slot="title">
+       np                 <template slot="title">
                             退出
                         </template>
                     </lay-menu-item>
@@ -57,36 +57,9 @@
                     </lay-menu-item>
                     <lay-menu-item :index="1">
                         <template slot="title">
-                            <span>布局</span>
+                            <span>员工个人月产量管理</span>
                         </template>
-                        <lay-menu-child-item title="栅格" :to="{name: 'grid'}"></lay-menu-child-item>
-                        <lay-menu-child-item title="后台布局" :to="{name: 'admin'}"></lay-menu-child-item>
-
-                    </lay-menu-item>
-                    <lay-menu-item :index="2">
-                        <template slot="title">
-                            <span>基本元素</span>
-                        </template>
-                        <lay-menu-child-item title="按钮" :to="{name: 'button'}"></lay-menu-child-item>
-                        <lay-menu-child-item title="表单" :to="{name: 'form'}"></lay-menu-child-item>
-                        <lay-menu-child-item title="导航/面包屑" :to="{name: 'nav'}"></lay-menu-child-item>
-                        <lay-menu-child-item title="选项卡" :to="{name: 'tabs'}"></lay-menu-child-item>
-                        <lay-menu-child-item title="进度条" :to="{name: 'progress'}"></lay-menu-child-item>
-                        <lay-menu-child-item title="面板" :to="{name: 'panel'}"></lay-menu-child-item>
-                        <lay-menu-child-item title="徽章" :to="{name: 'badge'}"></lay-menu-child-item>
-                        <lay-menu-child-item title="时间线" :to="{name: 'timeline'}"></lay-menu-child-item>
-                        <lay-menu-child-item title="静态表格" :to="{name: 'table'}"></lay-menu-child-item>
-                        <lay-menu-child-item title="动画" :to="{name: 'animation'}"></lay-menu-child-item>
-                        <lay-menu-child-item title="辅助元素" :to="{name: 'auxiliar'}"></lay-menu-child-item>
-                    </lay-menu-item>
-                    <lay-menu-item :index="3">
-                        <template slot="title">
-                            <span>组件示例</span>
-                        </template>
-                        <lay-menu-child-item title="弹出层" :to="{name: 'layer'}"></lay-menu-child-item>
-                        <lay-menu-child-item title="分页" :to="{name: 'laypage'}"></lay-menu-child-item>
-                        <lay-menu-child-item title="滑块" :to="{name: 'slider'}"></lay-menu-child-item>
-                        <lay-menu-child-item title="评分" :to="{name: 'rate'}"></lay-menu-child-item>
+                        <lay-menu-child-item title="员工个人月产量" :to="{name: 'admin'}"></lay-menu-child-item>
 
                     </lay-menu-item>
 
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,15 +1,51 @@
 <template>
-    <div>
-        <lay-block>
-            这就是！！！
-        </lay-block>
-    </div>
+  <div>
+    <lay-block>
+      具体数据以表格形式展示
+    </lay-block>
+
+    <lay-block title="数据详情" style="margin-top: 50px;"></lay-block>
+    <table border="1" width="500px">
+      <thead >
+      <tr>
+        <th>员工姓名</th>
+        <th>部件名称</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(item,i) in tableData" :key="i">
+        <td>{{item.name}}</td>
+        <td>
+          <table border="1">
+            <tr v-for="(product,index) in item.productEntities" :key="index">
+              <td>{{product.pname}}</td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-	export default {
-		name: "Admin"
-	}
+import Table from "@/views/Table";
+export default {
+  name: "Admin",
+  components: {Table},
+  data() {
+    return {
+      tableData: []
+    }
+  },
+  mounted:function () {
+    this.axios.get('http://192.168.2.106:18500/xst/user/findAll')
+        .then(response => {
+          console.log(response)
+          this.tableData = response.data.data
+        });
+  },
+}
 </script>
 
 <style scoped>
